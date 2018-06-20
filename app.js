b3cfce77598eb2604b05c95bd2cc4554c2e308bc
Vinicius Moura <viniciusmq@outlook.com>
2018-06-20 17:22:01 -0300

    Openshift Configuration
    All openShift configuration

--- app.js
+++ app.js
@@ -66,7 +66,9 @@ routes(app);
 // app.listen(process.env.PORT_APP, function() {
 //     console.log('Rodando porta' + process.env.PORT_APP);
 // });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(server_port ,server_ip_address, function(){
 console.log("Servidor rodando na porta %d em modo %s", this.address().port, app.settings.env);
 });
